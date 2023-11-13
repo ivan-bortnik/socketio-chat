a122a7f50df03dafe9458a146962772919af43f2
Ivan Bortnik <ivan.bortnik@etudiant.univ-lr.fr>
2023-11-13 10:59:56 +0100

    feat (global): connection to backend

--- frontend/socketio-chat-frontend/src/routes/+page.svelte
+++ frontend/socketio-chat-frontend/src/routes/+page.svelte
@@ -1,2 +1,13 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<script>
+
+// Connect to server
+import io from 'socket.io-client';
+const ENDPOINT = 'http://localhost:3000';
+
+const socket = io(ENDPOINT);
+
+</script>
