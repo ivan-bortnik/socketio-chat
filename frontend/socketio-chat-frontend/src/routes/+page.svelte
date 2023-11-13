59b0ee027e0d73240f4d61c2f103a79326fa48be
Ivan Bortnik <ivan.bortnik@etudiant.univ-lr.fr>
2023-11-13 11:43:47 +0100

    feat (frontend): basic frontend

--- frontend/socketio-chat-frontend/src/routes/+page.svelte
+++ frontend/socketio-chat-frontend/src/routes/+page.svelte
@@ -1,6 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="page-wrapper">
 
+    <ul class="chat-window">
+        {#each messages as msg, index}
+            {#if index == 0 || messages[index-1].username != messages[index].username}
+                <li class="username">{ msg.username }</li>
+            {/if}
+            <li class="chat-message">{ msg.content }</li>
+        {/each}
+    </ul>
+
+    <div class="message-input">
+        <input type="text" placeholder="Message...">
+        <button>Send</button>
+    </div>
+
+</div>
 
 <script>
 
@@ -10,4 +24,16 @@ const ENDPOINT = 'http://localhost:3000';
 
 const socket = io(ENDPOINT);
 
+var messages = [
+    {"username": "Bob", "content": "Lorem Ipsum"},
+    {"username": "Bob", "content": "Lorem Ipsum"},
+    {"username": "Bobette", "content": "Lorem Ipsum"},
+    {"username": "Bobette", "content": "Lorem Ipsum"},
+    {"username": "Bob", "content": "Lorem Ipsum"}
+]
+
 </script>
+
+<style>
+    @import "../../static/style.css";
+</style>
