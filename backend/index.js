863b65627dbef524769321401aa7b6807fe71a95
Ivan Bortnik <ivan.bortnik@etudiant.univ-lr.fr>
2023-11-15 14:53:10 +0100

    nique les CORS

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "netlify.app",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
